f71db9e175522e81c8a4f1262fb55d0b8688db2d
Mohd Mustkeem <mohdmustkeem78624@gmail.com>
2022-05-20 15:35:25 +0530

    Add project files.

---
+++ BookAPI/Models/BookContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+        public DbSet<Book> Books { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    const string cs = @"Server=DESKTOP-L91OLLD;Database=Books;Trusted_Connection=true";
+        //    optionsBuilder.UseSqlServer(cs);
+
+
+        //    using (var db = new BookContext())
+        //    {
+        //        db.Database.EnsureCreated();
+        //    }
+        //}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
